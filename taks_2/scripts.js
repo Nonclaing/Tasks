c149d2b5e15cf896734e4f4665459b554a6c5eea
Nomad0029 <32881294+Nonclaing@users.noreply.github.com>
2022-02-15 17:03:50 +0300

    Пофиксил баг
    Перед сном вспомнил, что не проверяю границу массива в цикле с j

--- taks_2/scripts.js
+++ taks_2/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     let elements = row.children();
     let count = elements.length;
 
+    changeHeight(row, elements, count);
     $(window).resize(function () {
         changeHeight(row, elements, count);
     })
@@ -13,7 +14,7 @@ function changeHeight (row, elements, count) {
     let elementWidth = elements.eq(0).outerWidth();
     let countInRow = Math.floor(size / elementWidth);
 
-    for (let i = 0; i < count; i += countInRow + 1) {
+    for (let i = 0; i < count ; i += countInRow + 1) {
         let maxHeight = findMaxInRow(elements, i, countInRow);
         SetHeight(elements, i, countInRow, maxHeight);
     }
@@ -29,7 +30,7 @@ function findMaxInRow(elements, firstElement, countInRow) {
     return maxHeight;
 }
 function SetHeight(elements, firstElement, countInRow, Height) {
-    for (let j = firstElement; j < countInRow; j++) {
+    for (let j = firstElement; j < countInRow && j < elements.length; j++) {
         elements.eq(j).height(Height);
     }
 }
